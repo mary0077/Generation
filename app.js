34d0b73830d5a3a53b5803dac191a0ec136db653
mary0077 <marianagomes201@hotmail.com>
2024-08-07 13:05:47 -0300

    Finalizacao do desafio final generation com swagger implementado

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/database');
+const alunosRouter = require('./routes/alunos');
+const setupSwagger = require('./swagger');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/alunos', alunosRouter);
+
+setupSwagger(app);
+
+sequelize.sync()
+  .then(() => {
+    console.log('Banco de dados conectado');
+    app.listen(3077, () => {
+      console.log('Servidor rodando na porta 3077');
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao conectar com o banco de dados:', err);
+  });
