84b9e4dd07859804b0914ca6397aff77e45c20ef
mary0077 <marianagomes201@hotmail.com>
2024-08-07 19:01:17 -0300

    config again vercel and deploy

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
+// config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('escola', 'adm', '123456', {
-  host: 'localhost',
-  dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Ajuste conforme necessário para seu banco de dados
+    }
+  }
 });
 
 module.exports = sequelize;
