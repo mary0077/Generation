3846a4e156e07359b481bda83dd23acefc0d750d
mary0077 <marianagomes201@hotmail.com>
2024-08-08 15:01:10 -0300

    test vercel

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false //Necessário por conta da nuvem
     }
   }
 });
